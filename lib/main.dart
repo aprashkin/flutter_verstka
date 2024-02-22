a3966fad340915c5ff555d8f059acbb471a727e8
aprashkin <dandrianov02@gmail.com>
2024-02-22 23:46:32 +0300

    myApp-1.2 (changes: add new file person_cards.dart with style for person cards)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:try_3/stylist_card/person_cards.dart';
+
 
 void main() {
   runApp(const TryThree());
@@ -6,6 +8,7 @@ void main() {
 
 class TryThree extends StatelessWidget {
   const TryThree({super.key});
+
 
 
     @override
@@ -16,7 +19,7 @@ class TryThree extends StatelessWidget {
             backgroundColor: const Color.fromARGB(255, 252, 248, 235),
             body: SafeArea(
               child: Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -25,7 +28,7 @@ class TryThree extends StatelessWidget {
                     children: [
                       InkWell(
                         onTap: () {},
-                        child: Icon(Icons.arrow_back_ios),
+                        child: const Icon(Icons.arrow_back_ios),
                       ),
                       IconButton(
                         onPressed: () {},
@@ -35,81 +38,32 @@ class TryThree extends StatelessWidget {
                   ),
                     const SizedBox(height: 10),
                     const Text(
-                    'Hair Stylist',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
+                      'Hair Stylist',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
                     ),
-                  ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          width: 150,
-                          height: 220,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white
-                          ),
-                          child: const Column(
-                            children: [
-                              Icon(Icons.face_retouching_natural,
-                              size: 80,
-                              ),
-                              Text('Anna Ray',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(Icons.star_rounded,
-                                  color: Colors.yellow,
-                                  size: 15),
-                                  Icon(Icons.star_rounded,
-                                    color: Colors.yellow,
-                                    size: 15),
-                                  Icon(Icons.star_rounded,
-                                    color: Colors.yellow,
-                                    size: 15),
-                                  Icon(Icons.star_rounded,
-                                    color: Colors.yellow,
-                                    size: 15),
-                                  Icon(Icons.star_rounded,
-                                    color: Colors.yellow,
-                                    size: 15),
-                                ],
-                              ),
-                              Text('5 years experience',
-                              style: TextStyle(
-                                fontSize: 10
-                                ),
-                              ),
-                              Row(
-                                //  children: [
-                                //   IconButton(
-                                //     onPressed: () {},
-                                //     icon: Icon(Icons.search_rounded, size: 30
-                                //     ),
-                                //   ),
-                                // ],
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: 150,
-                          height: 220,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white
-                          ),
+                    const SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child:  Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                           PerssonCard(personName: 'qwerty', experience: 5, rateStar: 5),
+                           SizedBox(width: 10),
+                           PerssonCard(personName: 'qwerty2', experience: 4, rateStar: 4),
+                           SizedBox(width: 10),
+                           PerssonCard(personName: 'qwerty3', experience: 3, rateStar: 3),
+                           SizedBox(width: 10),
+                           PerssonCard(personName: 'qwerty4', experience: 2, rateStar: 2),
+                           SizedBox(width: 10),
+                           PerssonCard(personName: 'qwerty5', experience: 1, rateStar: 1),
 
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
+                    )
+
+
                   ],
                 ),
               ),
---
+++ lib/stylist_card/person_cards.dart
@@ -0,0 +1,85 @@
+
+
+import 'package:flutter/material.dart';
+
+class PerssonCard extends StatelessWidget {
+  final String personName;
+  final int experience;
+  final int rateStar;
+
+  const PerssonCard({
+    super.key,
+    required this.personName,
+    required this.experience,
+    required this.rateStar
+  });
+
+  List<Icon> countStar()
+  {
+    List<Icon> ratingstar = [];
+
+    int i;
+    for (i = 0; i < rateStar; i++)
+    {
+      ratingstar.add(const Icon(Icons.star_rounded,
+          color: Colors.yellow,
+          size: 15),);
+    }
+    return ratingstar;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Container(
+
+        width: 170,
+        height: 220,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.white
+        ),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  const Icon(Icons.face_retouching_natural,
+                    size: 80,
+                  ),
+                  Text(personName,
+                    style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: countStar()
+                  ),
+                  Text('$experience years experience',
+                    style: const TextStyle(
+                        fontSize: 10
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu, size: 50
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+  }
+}
+
+
+
+
+
